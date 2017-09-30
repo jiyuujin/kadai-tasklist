f9456c2a85c3b162267adc77cae33c3921a5a97b
yuumakitamura <jiyuujinunite@gmail.com>
2017-09-30 02:55:56 +0000

    change main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="utf-8">
         <title>MessageBoard</title>
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
 
     <body>
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -2,14 +2,25 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <div class="content">
+        <h1>メッセージ一覧</h1>
+        <div class="pull-right">
+            <a href="{{ URL::route('messages.create') }}" class="btn btn-primary">新規</a>
+        </div>
 
-    @if (count($messages) > 0)
-        <ul>
-            @foreach ($messages as $message)
-                <li>{{ $message->content }}</li>
-            @endforeach
-        </ul>
-    @endif
+        @foreach ($messages as $message)
+            <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->content }}</li>
+        @endforeach
+    
+        <!--
+        @if (count($messages) > 0)
+            <ul>
+                @foreach ($messages as $message)
+                    <li>{{ $message->content }}</li>
+                @endforeach
+            </ul>
+        @endif
+        -->
+    </div>
 
 @endsection
